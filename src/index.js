2828131456979f539ae693fa6fb35da38cb92128
Bhupesh-aher <aherbhupesh18@gmail.com>
2025-03-05 15:53:29 +0530

    Added new migration to add foreign key constraint in one to many relation using the following command ``` npx sequelize migration:generate --name update-city-airport-association
    ```

--- src/index.js
+++ src/index.js
@@ -11,7 +11,23 @@ app.use(express.urlencoded({extended: true}))
 app.use('/api', apiRoutes)
 
 
-app.listen(serverConfig.PORT, () => {
+app.listen(serverConfig.PORT, async() => {
     console.log(`Successfully started the server on PORT: ${serverConfig.PORT}`);
-   
+   const {City, Airport} = require('./models')
+   const bangalaru = await City.findByPk(1);
+    // const dbpairport = await bangalaru.createAirport({name: 'Huballi Airport', code: 'HBL'})
+    
+    // const airportsInBlr = await bangalaru.getAirports();
+    // console.log(airportsInBlr);
+    
+    // const hbairport = await Airport.findByPk(2);
+    // console.log(hbairport);
+    
+    // await bangalaru.removeAirport(hbairport)
+    // await City.destroy({
+    //     where: {
+    //         id: 1
+    //     }
+    // });
+
 });     
