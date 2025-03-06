44f99473127c64befa96a8305c0687df2cc1bbd8
Bhupesh-aher <aherbhupesh18@gmail.com>
2025-03-06 14:45:07 +0530

    FEAT: added create flights api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const airplanesRoutes = require('./airplane-routes')
 const cityRoutes = require('./city-routes')
 const airportRoutes = require('./airport-routes')
+const flightRoutes = require('./flight-routes')
 
 const router = express.Router();
 
 router.use('/airplanes', airplanesRoutes)
 router.use('/cities', cityRoutes)
 router.use('/airports', airportRoutes)
+router.use('/flights', flightRoutes)
 
 
 module.exports = router;
--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     AirplaneServices: require('./airplane-service'),
     Cityservice: require('./city-service'),
-    Airportservice: require('./airport-service')
-
+    Airportservice: require('./airport-service'),
+    Flightservice: require('./flight-service')
 }
